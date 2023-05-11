dea7b795ba13a94bc7b7db3a0f1b672084eef1f7
kifarid <faridk@informatik.uni-freiburg.de>
2023-05-11 19:35:30 +0200

    last clsg run

--- sample_dvces_jobs.sh
+++ sample_dvces_jobs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N ldvces
+#PBS -N LDCE_clsg
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=8:gpus=1:nvidiaRTX3090,mem=15gb,walltime=24:00:00
 #PBS -o logs/
@@ -27,19 +27,19 @@ echo generating for $PBS_ARRAYID to $((PBS_ARRAYID+1))
 
 
 
-python -m scripts.dvce --config-name=v8_wider\
-    data.batch_size=4 \
-    output_dir=/misc/lmbraid21/faridk/LDCE_v8_cf_binning \
-    sampler.guidance=projected \
-    sampler.cone_projection_type=binning \
-    data.start_sample=$PBS_ARRAYID data.end_sample=$((PBS_ARRAYID+1)) > logs/cf_binning_$PBS_ARRAYID.log 
-
-# python -m scripts.dvce --config-name=v8\
+# python -m scripts.dvce --config-name=v8_wider\
 #     data.batch_size=4 \
-#     output_dir=/misc/lmbraid21/faridk/LDCE_v8_clsg_ws \
-#     sampler.guidance=non-projection \
-#     sampler.cone_projection_type=default \
-#     data.start_sample=$PBS_ARRAYID data.end_sample=$((PBS_ARRAYID+1)) > logs/cg_ws_$PBS_ARRAYID.log 
+#     output_dir=/misc/lmbraid21/faridk/LDCE_v8_cf_binning \
+#     sampler.guidance=projected \
+#     sampler.cone_projection_type=binning \
+#     data.start_sample=$PBS_ARRAYID data.end_sample=$((PBS_ARRAYID+1)) > logs/cf_binning_$PBS_ARRAYID.log 
+
+python -m scripts.dvce --config-name=v8_wider \
+    data.batch_size=5 \
+    output_dir=/misc/lmbraid21/faridk/LDCE_w382_cc23_clsg \
+    sampler.guidance=non-projection \
+    sampler.cone_projection_type=default \
+    data.start_sample=$PBS_ARRAYID data.end_sample=$((PBS_ARRAYID+1)) > logs/LDCE_w382_cc23_clsg_$PBS_ARRAYID.log 
 
 #resume=True \
 #    sampler.guidance=non-projection \
